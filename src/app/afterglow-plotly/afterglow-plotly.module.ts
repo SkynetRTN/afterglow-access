3f033a49ed78556754ce30cd718c8199e9e8ad2c
jhaislip <jhaislip@gmail.com>
2019-07-02 15:22:06 -0400

    plotly import error fix

--- src/app/afterglow-plotly/afterglow-plotly.module.ts
+++ src/app/afterglow-plotly/afterglow-plotly.module.ts
@@ -4,10 +4,6 @@ import { AfterglowPlotComponent } from './plot/afterglow-plot.component';
 
 import { PlotlyViaWindowModule } from 'angular-plotly.js';
 
-
-//see https://github.com/plotly/plotly.js/issues/3518
-//had to manually change plotly.js line 32481  from }(); to }.apply(self);
-
 @NgModule({
   declarations: [AfterglowPlotComponent],
   imports: [
