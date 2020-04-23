272c519787a9581cf743876dc78fecdff6268423
jhaislip <jhaislip@gmail.com>
2020-04-23 14:44:49 -0400

    AppConfig instead of environment

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppConfig } from './environments/environment';
 
 import 'hammerjs';
 
-if (environment.production) {
+if (AppConfig.production) {
   enableProdMode();
 }
 
