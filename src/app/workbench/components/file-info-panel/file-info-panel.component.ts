6c0c9c4e2065fbb5f964c24e13bc1df8e583f6bf
Joshua Haislip <jhaislip@gmail.com>
2020-09-28 12:53:41 -0400

    .

--- src/app/workbench/components/file-info-panel/file-info-panel.component.ts
+++ src/app/workbench/components/file-info-panel/file-info-panel.component.ts
@@ -27,6 +27,8 @@ import { Router } from "@angular/router";
 import { Store } from "@ngxs/store";
 import { datetimeToJd } from "../../../utils/skynet-astro";
 import { FileInfoPanelConfig } from '../../models/file-info-panel';
+import { BehaviorSubject, Subject, Observable, combineLatest } from 'rxjs';
+import { map, distinctUntilChanged, filter } from 'rxjs/operators';
 
 @Component({
   selector: "app-file-info-panel",
@@ -36,137 +38,174 @@ import { FileInfoPanelConfig } from '../../models/file-info-panel';
 })
 export class FileInfoToolsetComponent
   implements OnInit, AfterViewInit, OnDestroy {
-  @Input() file: ImageFile;
-  @Input() config: FileInfoPanelConfig;
+  @Input("file")
+  set file(file: ImageFile) {
+    this.file$.next(file);
+  }
+  get file() {
+    return this.file$.getValue();
+  }
+  private file$ = new BehaviorSubject<ImageFile>(null);
+  
+  @Input("config")
+  set config(config: FileInfoPanelConfig) {
+    this.config$.next(config);
+  }
+  get config() {
+    return this.config$.getValue();
+  }
+  private config$ = new BehaviorSubject<FileInfoPanelConfig>(null);
 
   @Output() configChange: EventEmitter<Partial<FileInfoPanelConfig>> = new EventEmitter();
 
   columnsDisplayed = ["key", "value", "comment"];
+  headerSummary$: Observable<Header>;
 
   constructor(
     private decimalPipe: DecimalPipe,
     private datePipe: DatePipe,
     store: Store,
     router: Router
-  ) {}
+  ) {
 
-  getHeaderSummary() {
-    if (!this.file || !this.file.header) return [];
-    let result: Header = [];
-    let width = getWidth(this.file);
-    let height = getHeight(this.file);
-    let hasWcs = this.file.wcs.isValid();
-    let degsPerPixel = getDegsPerPixel(this.file);
-    let startTime = getStartTime(this.file);
-    let expLength = getExpLength(this.file);
-    let centerTime = getCenterTime(this.file);
-    let telescope = getTelescope(this.file);
-    let object = getObject(this.file);
-    let filter = getFilter(this.file);
+    let header$ = this.file$.pipe(
+      filter(file => file != null),
+      map(file => file.header),
+      distinctUntilChanged()
+    )
 
-    let systemTimeZone: string = new Date().getTimezoneOffset().toString();
+    this.headerSummary$ = combineLatest(
+      header$,
+      this.config$
+    ).pipe(
+      map(([header, config]) => {
+        if (!header) return [];
+        let result: Header = [];
+        let width = getWidth(this.file);
+        let height = getHeight(this.file);
+        let hasWcs = this.file.wcs.isValid();
+        let degsPerPixel = getDegsPerPixel(this.file);
+        let startTime = getStartTime(this.file);
+        let expLength = getExpLength(this.file);
+        let centerTime = getCenterTime(this.file);
+        let telescope = getTelescope(this.file);
+        let object = getObject(this.file);
+        let filter = getFilter(this.file);
 
-    result.push({
-      key: "ID",
-      value: `${this.file.id}`,
-      comment: "",
-    });
+        let systemTimeZone: string = new Date().getTimezoneOffset().toString();
 
-    if (width && height) {
-      result.push({
-        key: "Size",
-        value: `${width} x ${height} pixels`,
-        comment: "",
-      });
-
-      if (degsPerPixel) {
-        let fovX = width * degsPerPixel;
-        let fovY = height * degsPerPixel;
-        let units = "degs";
-        if (fovX < 1 && fovY < 1) {
-          units = "arcmins";
-          fovX *= 60;
-          fovY *= 60;
-        }
         result.push({
-          key: "FOV",
-          value: `${this.decimalPipe.transform(
-            fovX,
-            "1.0-1"
-          )} x ${this.decimalPipe.transform(fovY, "1.0-1")} ${units}`,
+          key: "ID",
+          value: `${this.file.id}`,
           comment: "",
         });
 
-        if (startTime) {
+        if (width && height) {
           result.push({
-            key: "Start",
-            value: `${this.datePipe.transform(
-              startTime,
-              "yyyy-MM-dd HH:mm:ss z",
-              this.config.useSystemTime ? systemTimeZone : "UTC"
-            )} (${datetimeToJd(startTime)} JD)`,
+            key: "Size",
+            value: `${width} x ${height} pixels`,
             comment: "",
           });
+
+          if (degsPerPixel) {
+            let fovX = width * degsPerPixel;
+            let fovY = height * degsPerPixel;
+            let units = "degs";
+            if (fovX < 1 && fovY < 1) {
+              units = "arcmins";
+              fovX *= 60;
+              fovY *= 60;
+            }
+            result.push({
+              key: "FOV",
+              value: `${this.decimalPipe.transform(
+                fovX,
+                "1.0-1"
+              )} x ${this.decimalPipe.transform(fovY, "1.0-1")} ${units}`,
+              comment: "",
+            });
+
+            if (startTime) {
+              result.push({
+                key: "Start Time",
+                value: `${this.datePipe.transform(
+                  startTime,
+                  "yyyy-MM-dd HH:mm:ss z",
+                  this.config.useSystemTime ? systemTimeZone : "UTC"
+                )}`,
+                comment: "",
+              });
+              result.push({
+                key: "Start JD",
+                value: `${datetimeToJd(startTime)} JD`,
+                comment: "",
+              });
+            }
+
+            if (centerTime) {
+              result.push({
+                key: "Center Time",
+                value: `${this.datePipe.transform(
+                  centerTime,
+                  "yyyy-MM-dd HH:mm:ss z",
+                  this.config.useSystemTime ? systemTimeZone : "UTC"
+                )}`,
+                comment: "",
+              });
+              result.push({
+                key: "Center JD",
+                value: `${datetimeToJd(centerTime)} JD`,
+                comment: "",
+              });
+            }
+
+            if (telescope) {
+              result.push({
+                key: "Telescope",
+                value: `${telescope}`,
+                comment: "",
+              });
+            }
+
+            if (filter) {
+              result.push({
+                key: "Filter",
+                value: `${filter}`,
+                comment: "",
+              });
+            }
+
+            if (expLength !== undefined) {
+              result.push({
+                key: "Exp Length",
+                value: `${expLength}`,
+                comment: "",
+              });
+            }
+          }
         }
 
-        if (centerTime) {
-          result.push({
-            key: "Center",
-            value: `${this.datePipe.transform(
-              centerTime,
-              "yyyy-MM-dd HH:mm:ss z",
-              this.config.useSystemTime ? systemTimeZone : "UTC"
-            )} (${datetimeToJd(centerTime)} JD)`,
-            comment: "",
-          });
-        }
+        result.push({
+          key: "WCS",
+          value: hasWcs,
+          comment: "",
+        });
 
-        if (telescope) {
-          result.push({
-            key: "Telescope",
-            value: `${telescope}`,
-            comment: "",
-          });
-        }
-
-        if (filter) {
-          result.push({
-            key: "Filter",
-            value: `${filter}`,
-            comment: "",
-          });
-        }
-
-        if (expLength !== undefined) {
-          result.push({
-            key: "Exp Length",
-            value: `${expLength}`,
-            comment: "",
-          });
-        }
-      }
-    }
-
-    result.push({
-      key: "WCS",
-      value: hasWcs,
-      comment: "",
-    });
-
-    return result;
+        return result;
+      })
+    )
   }
+  ngOnInit() { }
 
-  ngOnInit() {}
+  ngOnDestroy() { }
 
-  ngOnDestroy() {}
-
-  ngAfterViewInit() {}
+  ngAfterViewInit() { }
 
   onShowRawHeaderChange($event: MatSlideToggleChange) {
-    this.configChange.emit({showRawHeader: $event.checked})
+    this.configChange.emit({ showRawHeader: $event.checked })
   }
 
   onUseSystemTimeChange($event: MatSlideToggleChange) {
-    this.configChange.emit({useSystemTime: $event.checked})
+    this.configChange.emit({ useSystemTime: $event.checked })
   }
 }
