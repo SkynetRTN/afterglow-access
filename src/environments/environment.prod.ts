593ebf216a654cab6acbd15892091771e42b15a8
jhaislip <jhaislip@gmail.com>
2021-03-05 15:37:41 -0500

    cleanup styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import { AfterglowEnv } from "./afterglow-env";
 export const env: AfterglowEnv = {
   production: true,
   environment: 'PROD',
-  version: '1.0.1',
+  version: '1.0.2',
   buildDate: 'Friday, March 5, 2021',
   coreVersion: 'v1',
   configUrl: 'afterglow.json',
