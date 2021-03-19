e029e57bbe097b395dd423be280e4b56e7e9c465
Joshua Haislip <jhaislip@gmail.com>
2021-03-19 14:28:50 -0400

    increment build number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ import { AfterglowEnv } from './afterglow-env';
 export const env: AfterglowEnv = {
   production: false,
   environment: 'LOCAL',
-  version: '1.0.2',
-  buildDate: 'Friday, March 5, 2021',
+  version: '1.0.3',
+  buildDate: 'Friday, March 19, 2021',
   coreVersion: 'v1',
   configUrl: 'afterglow.json',
   plugins: [NgxsLoggerPluginModule.forRoot()],
