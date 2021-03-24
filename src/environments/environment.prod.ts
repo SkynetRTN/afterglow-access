d807d038e93b003f7dd55723c2b481e694801fdf
jhaislip <jhaislip@gmail.com>
2021-03-24 13:08:44 -0400

    v1.0.5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ import { AfterglowEnv } from './afterglow-env';
 export const env: AfterglowEnv = {
   production: true,
   environment: 'PROD',
-  version: '1.0.4',
-  buildDate: 'Saturday, March 20, 2021',
+  version: '1.0.5',
+  buildDate: 'Wednesday, March 24, 2021',
   coreVersion: 'v1',
   configUrl: 'afterglow.json',
   plugins: [],
