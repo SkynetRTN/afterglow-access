0b2fcbdb99cd839e1ab4790d356a270bc2a36e79
Joshua Haislip <jhaislip@gmail.com>
2021-09-08 13:14:46 -0400

    v1.09

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -4,8 +4,8 @@ import { AfterglowEnv } from './afterglow-env';
 export const env: AfterglowEnv = {
   production: false,
   environment: 'DEV',
-  version: '1.0.8',
-  buildDate: 'Monday, April 5, 2021',
+  version: '1.0.9',
+  buildDate: 'Wednesday, September 8, 2021',
   coreVersion: 'v1',
   configUrl: 'afterglow.json',
   plugins: [NgxsLoggerPluginModule.forRoot()],
