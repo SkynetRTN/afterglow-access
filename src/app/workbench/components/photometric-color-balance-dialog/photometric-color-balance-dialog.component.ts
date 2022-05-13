4c9866bf62d16064a12e860dfed7cbafd96b88a8
Joshua Haislip <jhaislip@gmail.com>
2022-05-13 08:07:41 -0400

    .

--- src/app/workbench/components/photometric-color-balance-dialog/photometric-color-balance-dialog.component.ts
+++ src/app/workbench/components/photometric-color-balance-dialog/photometric-color-balance-dialog.component.ts
@@ -1,49 +1,88 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Store } from '@ngxs/store';
 import { LogMessage as NgxLogMessage } from 'ngx-log-monitor';
-import { timer } from 'rxjs';
+import { BehaviorSubject, combineLatest, merge, Subject, timer } from 'rxjs';
 import { map, take } from 'rxjs/operators';
+import { DataFilesState } from 'src/app/data-files/data-files.state';
+import { FieldCalibrationJob } from 'src/app/jobs/models/field-calibration';
+import { JobType } from 'src/app/jobs/models/job-types';
+import { isFieldCalibrationJob } from 'src/app/jobs/models/field-calibration';
+import { JobService } from 'src/app/jobs/services/job.service';
+import { toFieldCalibration, toPhotometryJobSettings, toSourceExtractionJobSettings } from '../../models/global-settings';
+import { WorkbenchState } from '../../workbench.state';
 
 @Component({
   selector: 'app-photometric-color-balance-dialog',
   templateUrl: './photometric-color-balance-dialog.component.html',
   styleUrls: ['./photometric-color-balance-dialog.component.scss']
 })
-export class PhotometricColorBalanceDialogComponent implements OnInit {
-  logs: NgxLogMessage[] = [
-    { message: 'A simple log message' },
-    { message: 'A success message', type: 'SUCCESS' },
-    { message: 'A warning message', type: 'WARN' },
-    { message: 'An error message', type: 'ERR' },
-    { message: 'An info message', type: 'INFO' },
-    { message: 'A simple log message' },
-    { message: 'A success message', type: 'SUCCESS' },
-    { message: 'A warning message', type: 'WARN' },
-    { message: 'An error message', type: 'ERR' },
-    { message: 'An info message', type: 'INFO' },
-    { message: 'A simple log message' },
-    { message: 'A success message', type: 'SUCCESS' },
-    { message: 'A warning message', type: 'WARN' },
-    { message: 'An error message', type: 'ERR' },
-    { message: 'An info message', type: 'INFO' },
-    { message: 'A simple log message' },
-    { message: 'A success message', type: 'SUCCESS' },
-    { message: 'A warning message', type: 'WARN' },
-    { message: 'An error message', type: 'ERR' },
-    { message: 'An info message', type: 'INFO' },
-  ];
-
-  logStream$ = timer(0, 1000).pipe(
-    take(this.logs.length),
-    map(i => this.logs[i])
-  );
+export class PhotometricColorBalanceDialogComponent implements OnInit, AfterViewInit {
+  logStream$ = new BehaviorSubject<{ message: string, type?: 'SUCCESS' | 'WARN' | 'ERR' | 'INFO' }>({ message: 'Calculating photometric zero point for each layer...' });
 
   constructor(public dialogRef: MatDialogRef<PhotometricColorBalanceDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public layerIds: string[],
-    private store: Store) { }
+    private store: Store,
+    private jobService: JobService) {
+    let settings = this.store.selectSnapshot(WorkbenchState.getSettings);
+    let photometryJobSettings = toPhotometryJobSettings(settings);
+    let sourceExtractionJobSettings = toSourceExtractionJobSettings(settings);
+    let fieldCalibration = toFieldCalibration(settings);
+
+    let job: FieldCalibrationJob = {
+      type: JobType.FieldCalibration,
+      id: null,
+      photometrySettings: photometryJobSettings,
+      sourceExtractionSettings: sourceExtractionJobSettings,
+      fieldCal: fieldCalibration,
+      fileIds: layerIds,
+      state: null,
+    };
+
+    let job$ = this.jobService.createJob(job);
+
+    job$.subscribe(
+      (job) => {
+
+        if (isFieldCalibrationJob(job)) {
+          if (['in_progress', 'pending'].includes(job.state.status)) {
+            if (job.state.progress) {
+              this.logStream$.next({ message: `${job.state.progress}% complete` })
+            }
+            else {
+              this.logStream$.next({ message: `Field calibration in progress.` })
+            }
+            return;
+          }
+
+          if (job.state.status == 'completed') {
+            if (job.result) {
+
+            }
+            else {
+              this.logStream$.next({ message: `Field calibration completed. Downloading result.` })
+            }
+          }
+
+        }
+
+
+
+
+      },
+      (error) => {
+
+      },
+      () => {
+
+      })
+  }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
 
   }
 
