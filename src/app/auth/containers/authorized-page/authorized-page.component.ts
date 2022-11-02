1b18a5f6076216b7efc7c36cc547e08d38f88052
Joshua Haislip <jhaislip@gmail.com>
2022-11-01 22:24:44 -0400

    bypass interceptor when getting user after successful authentication

--- src/app/auth/containers/authorized-page/authorized-page.component.ts
+++ src/app/auth/containers/authorized-page/authorized-page.component.ts
@@ -2,18 +2,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { of, Subscription } from 'rxjs';
 import { GetOAuthToken, LoginSuccess, CheckSession } from '../../auth.actions';
 import { Navigate } from '@ngxs/router-plugin';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse, HttpBackend, HttpClient } from '@angular/common/http';
 
 import * as moment from 'moment';
 import * as qs from 'query-string';
 import { HttpParams } from '@angular/common/http';
 import { AuthService } from '../../services/auth.service';
 import { env } from '../../../../environments/environment';
-import { CookieService } from 'ngx-cookie';
+import { CookieService } from 'ngx-cookie-service';
 import { AppState } from '../../../app.state';
 import { AfterglowConfigService } from '../../../afterglow-config.service';
+import { CoreApiResponse } from 'src/app/utils/core-api-response';
+import { CoreUser } from '../../models/user';
+import { getCoreApiUrl } from 'src/app/afterglow-config';
 
 @Component({
   selector: 'app-authorized-page',
@@ -22,6 +28,7 @@ import { AfterglowConfigService } from '../../../afterglow-config.service';
 })
 export class AuthorizedPageComponent implements OnInit, OnDestroy {
   private sub: Subscription;
+  private httpClient: HttpClient;
 
   constructor(
     private store: Store,
@@ -29,18 +36,38 @@ export class AuthorizedPageComponent implements OnInit, OnDestroy {
     private router: Router,
     private authService: AuthService,
     private cookieService: CookieService,
-    private config: AfterglowConfigService
-  ) {}
+    private config: AfterglowConfigService,
+    private handler: HttpBackend
+  ) {
+    this.httpClient = new HttpClient(handler)
+  }
 
   ngOnInit() {
     if (this.config.authMethod == 'cookie') {
+
       if (this.cookieService.get(this.config.authCookieName)) {
-        //
-        this.authService.getUser().subscribe(
+        // need to bypass the interceptor for this so we can detect authentication issues
+        this.httpClient.get<CoreApiResponse<CoreUser>>(`${getCoreApiUrl(this.config)}/user`).pipe(
+          catchError((error) => {
+
+
+            if (error instanceof HttpErrorResponse) {
+              switch ((<HttpErrorResponse>error).status) {
+                case 401: {
+                  console.log("FAILED LOGIN.  COULD NOT OBTAIN USER INFO AFTER REDIRECT TO AUTHORIZED ENDPOINT USING COOKIE: ", this.cookieService.get(this.config.authCookieName))
+                }
+              }
+            }
+            return of(null)
+          })
+        ).subscribe(
           (user) => {
-            localStorage.setItem('user', JSON.stringify(user.data));
-            localStorage.setItem('access_token', this.cookieService.get(this.config.authCookieName));
-            this.store.dispatch(new LoginSuccess());
+            if (user) {
+              localStorage.setItem('user', JSON.stringify(user.data));
+              localStorage.setItem('access_token', this.cookieService.get(this.config.authCookieName));
+              this.store.dispatch(new LoginSuccess());
+            }
+
           },
           (error) => {
             //this.cookieService.remove(appConfig.authCookieName);
