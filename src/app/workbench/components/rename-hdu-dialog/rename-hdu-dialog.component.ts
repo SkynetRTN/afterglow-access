b4bf6c23f478e285412455e414c262b82638096f
Joshua Haislip <jhaislip@gmail.com>
2022-11-03 09:00:38 -0400

    rename the file as well as the HDU for single-HDU files

--- src/app/workbench/components/rename-hdu-dialog/rename-hdu-dialog.component.ts
+++ src/app/workbench/components/rename-hdu-dialog/rename-hdu-dialog.component.ts
@@ -4,6 +4,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { isValidFilename } from '../../../utils/validators';
 import { IHdu } from 'src/app/data-files/models/data-file';
 import { AfterglowDataFileService } from '../../services/afterglow-data-files';
+import { DataFilesState } from 'src/app/data-files/data-files.state';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-rename-hdu-dialog',
@@ -17,7 +19,7 @@ export class RenameHduDialogComponent implements OnInit, AfterViewInit {
     name: new FormControl('', [Validators.required, isValidFilename()]),
   });
 
-  constructor(private dialogRef: MatDialogRef<RenameHduDialogComponent>, @Inject(MAT_DIALOG_DATA) private hdu: IHdu, private dataFileService: AfterglowDataFileService) {
+  constructor(private dialogRef: MatDialogRef<RenameHduDialogComponent>, @Inject(MAT_DIALOG_DATA) private hdu: IHdu, private dataFileService: AfterglowDataFileService, private store: Store) {
 
     this.nameForm.get('name').setValue(hdu.name);
     this.nameForm.updateValueAndValidity();
@@ -32,8 +34,11 @@ export class RenameHduDialogComponent implements OnInit, AfterViewInit {
   save() {
     if (!this.nameForm.valid) return;
 
+    let file = this.store.selectSnapshot(DataFilesState.getFileByHduId(this.hdu.id));
+
+
     let name = this.nameForm.get('name').value;
-    this.dataFileService.updateFile(this.hdu.id, { name: name }).subscribe(() => {
+    this.dataFileService.updateFile(this.hdu.id, { name: name, groupName: file.hduIds.length == 1 ? name : file.name }).subscribe(() => {
       this.dialogRef.close({
         ...this.hdu,
         name: name
