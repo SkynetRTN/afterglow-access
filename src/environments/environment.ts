415609f6ba28a62d5ba2a46cc0b3229c9901634a
Joshua Haislip <jhaislip@gmail.com>
2023-03-17 21:41:25 -0400

    version patch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ import { AfterglowEnv } from './afterglow-env';
 export const env: AfterglowEnv = {
   production: false,
   environment: 'LOCAL',
-  version: '1.0.21',
-  buildDate: 'Friday, March 10, 2023',
+  version: '1.0.22',
+  buildDate: 'Friday, March 17, 2023',
   coreVersion: 'v1',
   configUrl: 'afterglow.json',
   plugins: [NgxsLoggerPluginModule.forRoot()],
